752249a10dee10f4676e3487fb47a79c6e85ebb3
oshingate <oshingate@gmail.com>
2021-05-26 13:04:29 +0530

    Assignment:validators in mongoose Schema

---
+++ block-BNaacj/simple/server.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let logger = require('morgan');
+let mongoose = require('mongoose');
+
+let app = express();
+
+mongoose.connect('mongodb://localhost:27017/test', (err) => {
+  console.log(err ? err : 'database connected');
+});
+
+app.listen(3000, () => {
+  console.log('server is live on port 3000');
+});
